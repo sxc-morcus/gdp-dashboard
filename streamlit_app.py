fe03e04936f7a41330c155808270774d6ac8f2c6
sxc-morcus <60882922+sxc-morcus@users.noreply.github.com>
2024-09-13 10:20:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title('My First Streamlit App')
 # Add a welcome message 
 st.write('Welcome to my Streamlit app!') 
 # Create a text input 
-widgetuser_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
+user_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
 # Display the customized message 
 st.write('Customized Message:', user_input)
