e26f312468d8c87203bb6b5e4f7e403f913a9b3d
sxc-morcus <60882922+sxc-morcus@users.noreply.github.com>
2024-09-20 09:14:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ def get_gdp_data():
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
     DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
+    DATA_FILENAME2 = path(__file__).parent/'data/data.csv'
     raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
     MIN_YEAR = 1960
